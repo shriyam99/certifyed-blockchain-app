f1428a3be69c23b4894c6f45328b74964f475f8c
shriyam99 <tripathishriyam99@gmail.com>
2020-05-05 21:35:36 +0530

    initial commit

---
+++ src/components/Sample.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Sample extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello react developer</h1>
+            </div>
+        );
+    }
+}
+
+export default Sample;
